b98b8d21c7b3de11ec06a4a71cfc196393efebc3
“Augsut-Rushme” <864011713@qq.com>
2021-05-16 22:07:38 +0800

    完成了购物车的第2次渲染！

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,5 +1,17 @@
 <template>
-  <view>cart1</view>
+  <view class="cart-container">
+    <my-address></my-address>
+  	<view class="cart-title">
+  	    <!-- 左侧的图标 -->
+  	  <uni-icons type="shop" size="18"></uni-icons>
+  	   <!-- 描述文本 -->
+  	  <text class="cart-title-text">购物车</text>
+  	</view>
+   <block v-for="(goods, i) in cart" :key="i">
+      <my-goods :goods="goods"></my-goods>
+    </block>
+       <my-settle></my-settle>
+    </view>
 </template>
 
 <script>
@@ -14,4 +26,31 @@
  }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .cart-title {
+    height: 40px;
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    padding-left: 5px;
+    border-bottom: 1px solid #efefef;
+    .cart-title-text {
+      margin-left: 10px;
+    }
+  }
+  .goods-item-left {
+    margin-right: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  
+    .goods-pic {
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+  }
+  .cart-container {
+    padding-bottom: 50px;
+  }
+</style>
---
+++ store/user.js
@@ -0,0 +1,53 @@
+export default {
+  // 开启命名空间
+  namespaced: true,
+
+  // state 数据
+  state: () => ({
+    // 3. 读取本地的收货地址数据，初始化 address 对象
+    address: JSON.parse(uni.getStorageSync('address') || '{}'),
+       token: uni.getStorageSync('token') || '',
+        userinfo: JSON.parse(uni.getStorageSync('userinfo') || '{}'),
+  }),
+
+  // 方法
+  mutations: {
+    // 更新收货地址
+    updateAddress(state, address) {
+      state.address = address
+
+      // 2. 通过 this.commit() 方法，调用 m_user 模块下的 saveAddressToStorage 方法将 address 对象持久化存储到本地
+      this.commit('m_user/saveAddressToStorage')
+    },
+    // 1. 定义将 address 持久化存储到本地 mutations 方法
+    saveAddressToStorage(state) {
+      uni.setStorageSync('address', JSON.stringify(state.address))
+    },
+      updateUserInfo(state, userinfo) {
+        state.userinfo = userinfo
+        // 通过 this.commit() 方法，调用 m_user 模块下的 saveUserInfoToStorage 方法，将 userinfo 对象持久化存储到本地
+        this.commit('m_user/saveUserInfoToStorage')
+      },
+       saveUserInfoToStorage(state) {
+          uni.setStorageSync('userinfo', JSON.stringify(state.userinfo))
+        },
+          updateToken(state, token) {
+            state.token = token
+            // 通过 this.commit() 方法，调用 m_user 模块下的 saveTokenToStorage 方法，将 token 字符串持久化存储到本地
+            this.commit('m_user/saveTokenToStorage')
+          },
+        
+          // 将 token 字符串持久化存储到本地
+          saveTokenToStorage(state) {
+            uni.setStorageSync('token', state.token)
+          }
+  },
+
+  // 数据包装器
+  getters: {addstr(state) {
+        if (!state.address.provinceName) return ''
+    
+        // 拼接 省，市，区，详细地址 的字符串并返回给用户
+        return state.address.provinceName + state.address.cityName + state.address.countyName + state.address.detailInfo
+      }},
+}
